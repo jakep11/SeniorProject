f67b9f208b0d514b4b78d0f287cf452ca7659574
Brady Shutt <bradyshutt@gmail.com>
2018-02-27 11:20:00 -0800

    .

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const topicRoutes = require('./topic.js');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.use('/Topic', topicRoutes);
+
 
 module.exports = router;
