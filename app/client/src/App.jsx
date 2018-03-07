40045f3c6efebed282fe54d801262499d3583997
Brady Shutt <bradyshutt@gmail.com>
2018-03-07 08:58:36 -0800

    rename .js to .jsx

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {users: []}
+  state = {users: []};
 
   componentDidMount() {
     fetch('/users')
