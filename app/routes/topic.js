d93df02fb94d7d7f93d1f7aa064b3ec33fcf0675
mle17 <michael.vu.le@hotmail.com>
2018-03-10 16:09:21 -0800

    Create stub functions for Topic

--- app/routes/topic.js
+++ app/routes/topic.js
@@ -1,18 +1,94 @@
-
 const Express = require('express');
 const router = Express.Router({caseSensitive: true});
+const async = require('async');
+const Tags = require('./Validator.js').Tags;
 
 router.baseURL = '/Topic';
 
+router.get('/', (req, res) => {
+   const vld = req.validator;
+   const sectionId = req.query.sectionId;
+   const cnn = req.cnn;
 
-router.route('')
+   const where = sectionId ? `WHERE sectionId = ${sectionId}` : '';
+   const query = `SELECT id, departmentId, name, sectionId FROM Topic ${where}`;
 
-  .get((req, res) => {
-    let where = req.query['sectionId'] ? `WHERE sectionId = ${req.query['sectionId']}` : '';
-    let query = `SELECT id, departmentId, name, sectionId FROM Topic ${where}`;
+   async.waterfall([
+      function validateAndQuery(cb) { // TODO: find out if we want all sections if no query
+         if (isNaN(sectionId)) { // query sectionId is not a number
+            res.status(400).end();
+            cb(true);
+         }
+         else {
+            cnn.chkQry(query, null, cb);
+         }
+      },
 
-    res.json();
-  });
+      function checkResults(topicResults, fields, cb) {
+         if (vld.check(topicResults.length, Tags.notFound, null, cb)) {
+            res.json(topicResults);
+            cb(null);
+         }
+      }
+   ], function(err) {
+      if (err) 
+         res.json(err);
+      
+      cnn.release();
+   });
+});
 
+router.post('/', (req, res) => {
+   const vld = req.validator;
+   const cnn = req.cnn;
+   const body = req.body;
+
+   const postTopicFields = ['name', 'sectionId'];
+   const getSectionQuery = '';
+   const insertTopicQuery = '';
+
+   async.waterfall([
+      function checkSectionExists(cb) { // validate input and check section exists
+         if (vld.hasFields(body, postTopicFields, cb) && 
+            vld.hasOnlyFields(body, postTopicFields, cb)) { // validate input
+
+            cnn.chkQry(getSectionQuery, [body.sectionId], cb);
+         }
+      },
+
+      function createTopic(sectionResults, fields, cb) { // insert topic into DB
+         if (vld.check(sectionResults.length, Tags.notFound, null, cb)) {
+            cnn.chkQry(insertTopicQuery, body, cb);
+         }
+      },
+
+      function finalizeResponse(insertResult, fields, cb) { // finalize response
+         res.location(router.baseURL + '/' + insertResult.insertId).end();
+         cb(null);
+      }
+      
+   ], function(err) {
+      if (err) 
+         res.json(err);
+      
+      cnn.release();
+   });
+});
+
+router.get('/:id', (req, res) => {
+
+});
+
+router.put('/:id', (req, res) => {
+
+});
+
+router.delete('/:id', (req, res) => {
+
+});
+
+router.get('/:id/Activities', (req, res) => {
+
+});
 
 module.exports = router;
