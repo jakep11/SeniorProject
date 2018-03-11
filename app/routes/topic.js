103c6abf68cd818fa4daa6248f91f78e7a7fb6c0
mle17 <michael.vu.le@hotmail.com>
2018-03-10 16:51:37 -0800

    Add topic route implementations w/ placeholder queries

--- app/routes/topic.js
+++ app/routes/topic.js
@@ -6,7 +6,6 @@ const Tags = require('./Validator.js').Tags;
 router.baseURL = '/Topic';
 
 router.get('/', (req, res) => {
-   const vld = req.validator;
    const sectionId = req.query.sectionId;
    const cnn = req.cnn;
 
@@ -14,7 +13,7 @@ router.get('/', (req, res) => {
    const query = `SELECT id, departmentId, name, sectionId FROM Topic ${where}`;
 
    async.waterfall([
-      function validateAndQuery(cb) { // TODO: find out if we want all sections if no query
+      function validateAndQuery(cb) { 
          if (isNaN(sectionId)) { // query sectionId is not a number
             res.status(400).end();
             cb(true);
@@ -25,12 +24,11 @@ router.get('/', (req, res) => {
       },
 
       function checkResults(topicResults, fields, cb) {
-         if (vld.check(topicResults.length, Tags.notFound, null, cb)) {
-            res.json(topicResults);
-            cb(null);
-         }
+         res.json(topicResults);
+         cb(null);
       }
-   ], function(err) {
+
+   ], function callback(err) {
       if (err) 
          res.json(err);
       
@@ -67,7 +65,7 @@ router.post('/', (req, res) => {
          cb(null);
       }
       
-   ], function(err) {
+   ], function callback(err) {
       if (err) 
          res.json(err);
       
@@ -76,19 +74,151 @@ router.post('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
+   const cnn = req.cnn;
+   const topicId = req.params.id;
 
+   const getTopicQuery = '';
+
+   async.waterfall([
+      function checkTopicExists(cb) { // validate input and check section exists
+         if (isNaN(topicId)) { // query sectionId is not a number
+            res.status(400).end();
+            cb(true);
+         }
+         else {
+            cnn.chkQry(getTopicQuery, [topicId], cb);
+         }
+      },
+
+      function finalizeResponse(topicResults, fields, cb) { // finalize response
+         res.json(topicResults);
+         cb(null);
+      }
+      
+   ], function callback(err) {
+      if (err) 
+         res.json(err);
+      
+      cnn.release();
+   });
 });
 
 router.put('/:id', (req, res) => {
+   const vld = req.validator;
+   const cnn = req.cnn;
+   const body = req.body;
+   const topicId = req.params.id;
 
+   const putTopicFields = ['name', 'sectionId'];
+   const getTopicQuery = '';
+   const updateTopicQuery = '';
+
+   async.waterfall([
+      function checkTopicExists(cb) { // validate input and check section exists
+         if (isNaN(topicId)) { // query sectionId is not a number
+            res.status(400).end();
+            cb(true);
+         }
+         else if (vld.checkAdmin(cb) &&
+            vld.hasOnlyFields(body, putTopicFields, cb)) { // validate input
+
+            cnn.chkQry(getTopicQuery, [topicId], cb);
+         }
+      },
+
+      function updateTopic(topicResults, fields, cb) { // update topic in DB
+         if (vld.check(topicResults.length, Tags.notFound, null, cb)) {
+            cnn.chkQry(updateTopicQuery, body, cb);
+         }
+      },
+
+      function finalizeResponse(updateResult, fields, cb) { // finalize response
+         res.status(200).end();
+         cb(null);
+      }
+      
+   ], function callback(err) {
+      if (err) 
+         res.json(err);
+      
+      cnn.release();
+   });
 });
 
 router.delete('/:id', (req, res) => {
+   const vld = req.validator;
+   const cnn = req.cnn;
+   const topicId = req.params.id;
 
+   const getTopicQuery = '';
+   const deleteTopicQuery = '';
+
+   async.waterfall([
+      function checkTopicExists(cb) { // validate input and check section exists
+         if (isNaN(topicId)) { // query sectionId is not a number
+            res.status(400).end();
+            cb(true);
+         }
+         else if (vld.checkAdmin(cb)) {
+            cnn.chkQry(getTopicQuery, [topicId], cb);
+         }
+      },
+
+      function deleteTopic(topicResults, fields, cb) { // delete topic from DB
+         if (vld.check(topicResults.length, Tags.notFound, null, cb)) {
+            cnn.chkQry(deleteTopicQuery, [topicId], cb);
+         }
+      },
+
+      function finalizeResponse(deleteResult, fields, cb) { // finalize response
+         res.status(200).end();
+         cb(null);
+      }
+      
+   ], function callback(err) {
+      if (err) 
+         res.json(err);
+      
+      cnn.release();
+   });
 });
 
 router.get('/:id/Activities', (req, res) => {
+   const vld = req.validator;
+   const cnn = req.cnn;
+   const topicId = req.params.id;
 
+   const getTopicQuery = '';
+   const getTopicActivitiesQuery = '';
+
+   async.waterfall([
+      function checkTopicExists(cb) { // validate input and check section exists
+         if (isNaN(topicId)) { // query sectionId is not a number
+            res.status(400).end();
+            cb(true);
+         }
+         else if (vld.checkAdmin(cb)) {
+            cnn.chkQry(getTopicQuery, [topicId], cb);
+         }
+      },
+
+      function getTopicActivities(topicResults, fields, cb) { // get topic's activities from DB
+         if (vld.check(topicResults.length, Tags.notFound, null, cb)) {
+            cnn.chkQry(getTopicActivitiesQuery, [topicId], cb);
+         }
+      },
+
+      function finalizeResponse(topicActivitiesResult, fields, cb) { // finalize response
+         res.json(topicActivitiesResult);
+         cb(null);
+      }
+      
+   ], function callback(err) {
+      if (err) 
+         res.json(err);
+      
+      cnn.release();
+   });
 });
 
 module.exports = router;
