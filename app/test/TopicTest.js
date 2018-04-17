c6547ba7fd1d382f9818c939b5f3a753ff2d4c43
mle17 <michael.vu.le@hotmail.com>
2018-04-16 19:49:08 -0700

    Correct test to state badValue instead of missingField for PUT.

--- app/test/TopicTest.js
+++ app/test/TopicTest.js
@@ -543,10 +543,11 @@ describe('Topic Management', () => {
              .put('/Topic/' + '1')
              .send(newTopic3)
              .end((err, res) => {
+               console.log("PUT w/ null name: " + JSON.stringify(res.body));
                res.should.have.status(400);
                res.body.should.be.a('array');
                res.body.should.have.lengthOf(1);
-               res.body[0].should.have.property('tag', 'missingField');
+               res.body[0].should.have.property('tag', 'badValue');
                res.body[0].should.have.property('params');
                res.body[0].params.should.be.a('array');
                res.body[0].params.should.have.lengthOf(1);
