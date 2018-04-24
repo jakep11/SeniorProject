c29234e4b4edee49eb32ed0c04bc2e4036473092
KenOshima <koshima@calpoly.edu>
2018-04-24 08:17:39 -0700

    Working on Settings page.

--- app/client/src/components/Login/Login.jsx
+++ app/client/src/components/Login/Login.jsx
@@ -21,6 +21,7 @@ export default class Login extends Component {
 
    handleChange(event) {
       this.setState({[event.target.name]: event.target.value});
+      console.log("New state:", this.state);
    }
 
    submit() {
--- app/client/src/components/Settings/Settings.jsx
+++ app/client/src/components/Settings/Settings.jsx
@@ -5,12 +5,49 @@ import './Settings.css';
 export default class Settings extends Component {
    constructor(props) {
       super(props);
+      this.state = {
+         newUsername: null,
+         newPassword: null,
+         oldPassword: null,
+         oldPasswordAgain: null
+      };
+      
+      this.handleChange = this.handleChange.bind(this);
+   }
+
+   handleChange(event) {
+      this.setState({[event.target.name]: event.target.value});
+   }
+   
+   submit() {
+      if (this.state.oldPassword !== this.state.oldPasswordAgain) {
+         console.log('Bad passwords');
+      }
+      console.log('Changing information');
+      
    }
 
    render() {
       return (
          <div className='settings-wrapper'>
             <h1>Settings</h1>
+            <div className="settings-body">
+               <span>
+                  Enter new password:&nbsp;
+                  <input type="text" placeholder="New password" name="newPassword" onKeyUp={this.handleChange} />
+               </span>
+               <span>
+                  Enter old password:&nbsp;
+                  <input type="text" placeholder="Old password" name="oldPassword" onKeyUp={this.handleChange} />
+               </span>
+               <span>
+                  Enter old password again:&nbsp;
+                  <input type="text" placeholder="Old password again" name="oldPasswordAgain" onKeyUp={this.handleChange} />
+               </span>
+               <span>
+                  <button type="button" onClick={() => this.submit()}>Submit</button>
+               </span>
+            </div>
          </div>
       );
 
