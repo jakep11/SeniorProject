f4c5ca7f32fc90c0f0b5f38e053fd24d6c1084ee
KenOshima <koshima@calpoly.edu>
2018-04-24 06:22:30 -0700

    Cleaned up About page

--- app/client/src/components/About/About.jsx
+++ app/client/src/components/About/About.jsx
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import { Panel } from 'react-bootstrap';
+import './About.css';
 
 export default class About extends Component {
    constructor(props) {
@@ -9,9 +10,22 @@ export default class About extends Component {
 
    render() {
       return (
-         <div>
+         <div className="help-wrapper">
             <h1>About</h1>
-            <Panel>
+            <div>
+               This application is built as a senior project by 6 Cal Poly students.
+               The goal of the application is to provide an online learning platform that 
+               is customizable for teachers to use.
+            </div>
+            <div>
+               If you find an issue with the application, please report it&nbsp;
+               <a target="_blank" href="https://github.com/jakep11/SeniorProject/issues/new">
+                  here
+               </a>. 
+            </div>
+            
+            
+            {/*<Panel>
                <Panel.Body>
                This application is built as a senior project by 6 Cal Poly students.
                The goal of the application is to provide an online learning platform that 
@@ -22,7 +36,7 @@ export default class About extends Component {
                   here
                </a>. 
                </Panel.Body>
-            </Panel>
+            </Panel>*/}
          </div>
       )
    }
