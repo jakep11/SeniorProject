bf3bd65fcbe0bfda799c0912649b216a31761f15
Jake Pickett <j.pickett67@yahoo.com>
2018-05-23 13:18:51 -0700

    Adding more error checking to Progress code.

--- app/routes/Progress.js
+++ app/routes/Progress.js
@@ -58,10 +58,14 @@ router.put('/:userId', function (req, res) {
       },
       function (userArr, fields, cb) { // Check that the userId is valid
          if (userArr.length) {
+            let actT = body.activityType;
             if (vld.hasOnlyFields(body, vldFields, cb) &&
                vld.chain(body.activityId, Tags.missingField, ['activityId'])
                   .chain(body.activityType, Tags.missingField, ['activityType'])
-                  .check(body.grade, Tags.missingField, ['grade'], cb)) {
+                  .chain(typeof(body.grade) !== 'undefined', Tags.missingField, ['grade'])
+                  .chain(body.grade >= 0, Tags.badValue, ['grade'])
+                  .chain(body.activityId > 0, Tags.badValue, ['activityId'])
+                  .check(actT === 1 || actT === 2 || actT === 3, Tags.badValue, ['activityType'], cb)) {
                      body.attempted = true;
                      body.whenCompleted = body.grade !== 0 ? new Date() : null;
                      cnn.chkQry('UPDATE Progress SET ? WHERE activityType = ? AND activityId = ? AND userId = ?', [body, body.activityType, body.activityId, userId], cb);
