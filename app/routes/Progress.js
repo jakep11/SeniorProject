bbb886dc49b30e4cebffb0396fe787206ea2fce7
Jake Pickett <j.pickett67@yahoo.com>
2018-05-23 16:05:16 -0700

    Updated Progress to only update WhenCompleted when there is a grade other than 0 and it is null.

--- app/routes/Progress.js
+++ app/routes/Progress.js
@@ -62,18 +62,30 @@ router.put('/:userId', function (req, res) {
             if (vld.hasOnlyFields(body, vldFields, cb) &&
                vld.chain(body.activityId, Tags.missingField, ['activityId'])
                   .chain(body.activityType, Tags.missingField, ['activityType'])
-                  .chain(typeof(body.grade) !== 'undefined', Tags.missingField, ['grade'])
+                  .chain(typeof (body.grade) !== 'undefined', Tags.missingField, ['grade'])
                   .chain(body.grade >= 0, Tags.badValue, ['grade'])
                   .chain(body.activityId > 0, Tags.badValue, ['activityId'])
                   .check(actT === 1 || actT === 2 || actT === 3, Tags.badValue, ['activityType'], cb)) {
-                     body.attempted = true;
-                     body.whenCompleted = body.grade !== 0 ? new Date() : null;
-                     cnn.chkQry('UPDATE Progress SET ? WHERE activityType = ? AND activityId = ? AND userId = ?', [body, body.activityType, body.activityId, userId], cb);
+               cnn.chkQry('SELECT * from Progress WHERE userId = ? AND activityId = ? AND activityType = ?',
+                  [userId, body.activityId, body.activityType], cb);
             }
          }
          else {
             res.status(404).end();
             cb();
+         }
+      },
+      function (progressArr, fields, cb) {
+         if (progressArr.length) {
+            body.attempted = true;
+            if (((body.grade !== 0) && !progressArr[0].whenCompleted)) {
+               body.whenCompleted = new Date();
+            }
+            cnn.chkQry('UPDATE Progress SET ? WHERE activityType = ? AND activityId = ? AND userId = ?',
+               [body, body.activityType, body.activityId, userId], cb);
+         }
+         else {
+            vld.check(false, Tags.progressNotFound, null, cb);
          }
       },
       function (putResult, fields, cb) {
