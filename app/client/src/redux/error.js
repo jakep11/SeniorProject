dfbea025149cdeeea8604ee014b66ccacfa4d958
KenOshima <koshima@calpoly.edu>
2018-06-04 21:19:31 -0700

    Fixed error message popup on initial app startup, I think.

--- app/client/src/redux/error.js
+++ app/client/src/redux/error.js
@@ -5,10 +5,7 @@ export const SET_ERROR = 'SET_ERROR';
 export const CLEAR_ERROR = 'CLEAR_ERROR';
 
 
-const initialState = {
-   message: '',
-   style: 'danger'
-};
+const initialState = {};
 
 /* Reducer */
 export default function Error(state = initialState, action) {
