489fb7f9e4fabc63fbad9d538689c85094d96926
Bibek Shrestha <Bibek@Bibeks-MacBook-Pro.local>
2018-06-07 11:27:51 -0700

    Updated progress rows with sample values

--- app/database/populateProgress.sql
+++ app/database/populateProgress.sql
@@ -21,3 +21,49 @@ INSERT INTO Progress (activityType, userId, activityId, topicId)
   (SELECT e.userId, t.id as topicId, t.sectionId 
    FROM Enrollment e JOIN Topic t ON e.sectionId=t.sectionId) a 
     ON v.topicId = a.topicId;
+
+-- Updates
+-- Document
+Update Progress SET attempted = TRUE, grade = TRUNCATE(RAND(), 3)
+WHERE userId = 4 and activityType = 1 and 
+ (activityId <= 7) or (activityId >= 21 and activityId <= 23);
+Update Progress SET attempted = TRUE, grade = TRUNCATE(RAND(), 3)
+WHERE userId = 5 and activityType = 1 and 
+ (activityId % 2 = 0);
+ Update Progress SET attempted = TRUE, grade = TRUNCATE(RAND(), 3) 
+WHERE userId = 3 and activityType = 1 and 
+ (activityId % 2 = 1);
+
+Update Progress p Join Document d on d.id = p.activityId
+SET p.whenCompleted = DATE_SUB(d.dueDate, INTERVAL RAND()*5 HOUR)
+WHERE p.attempted = TRUE and activityType = 1;
+
+-- Exercise
+Update Progress SET attempted = TRUE, grade = TRUNCATE(RAND(), 3) 
+WHERE userId = 4 and activityType = 2 and 
+ (activityId <= 7) or (activityId >= 21 and activityId <= 23);
+Update Progress SET attempted = TRUE, grade = TRUNCATE(RAND(), 3)
+WHERE userId = 5 and activityType = 2 and 
+ (activityId % 2 = 0);
+ Update Progress SET attempted = TRUE, grade = TRUNCATE(RAND(), 3)
+WHERE userId = 3 and activityType = 2 and 
+ (activityId % 2 = 1);
+
+Update Progress p Join Exercise e on e.id = p.activityId
+SET p.whenCompleted = DATE_SUB(e.dueDate, INTERVAL RAND()*5 HOUR)
+WHERE p.attempted = TRUE and activityType = 2;
+
+-- Video
+Update Progress SET attempted = TRUE, grade = TRUNCATE(RAND(), 3) 
+WHERE userId = 4 and activityType = 3 and 
+ (activityId <= 7) or (activityId >= 21 and activityId <= 23);
+Update Progress SET attempted = TRUE, grade = TRUNCATE(RAND(), 3) 
+WHERE userId = 5 and activityType = 3 and 
+ (activityId % 2 = 0);
+Update Progress SET attempted = TRUE, grade = TRUNCATE(RAND(), 3)
+WHERE userId = 3 and activityType = 3 and 
+ (activityId % 2 = 1);
+
+Update Progress p Join Video v on v.id = p.activityId
+SET p.whenCompleted = DATE_SUB(v.dueDate, INTERVAL RAND()*5 HOUR)
+WHERE p.attempted = TRUE and activityType = 3;
